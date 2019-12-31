e7819700776942adcc5dc126721312c235375e28
TieuNhiKk <hienvh00@gmail.com>
2019-12-31 08:11:23 +0700

    [Bài tập] Game đơn giản - di chuyển đối tượng

---
+++ Week3/Game-don-gian-di-chuyen-doi-tuong/script.js
@@ -0,0 +1,80 @@
+/**
+ * Created by nhatnk on 4/26/17.
+ */
+
+function Hero(image, top, left, size) {
+  this.image = image;
+  this.top = top;
+  this.left = left;
+  this.size = size;
+
+  this.getHeroElement = function () {
+    return '<img width="' + this.size + '"' +
+      ' height="' + this.size + '"' +
+      ' src="' + this.image + '"' +
+      ' style="top: ' + this.top + 'px; left:' + this.left + 'px;position:absolute;" />';
+  }
+
+  this.moveRight = function () {
+    this.left += 10;
+    console.log('ok: ' + this.left);
+  }
+  this.moveLeft = function () {
+    this.left -= 10;
+  }
+  this.moveDown = function () {
+    this.top += 10;
+  }
+  this.moveUp = function () {
+    this.top -= 10;
+  }
+}
+
+let hero = new Hero('Donald.png', 20, 30, 200);
+let width = window.innerWidth - hero.size;
+let height=window.innerHeight-hero.size;
+function start() {
+  console.log("TCL: start -> window.innerWidth-hero.size", window.innerWidth - hero.size);
+  if (hero.left < width) {
+    hero.moveRight();
+    document.getElementById('game').innerHTML = hero.getHeroElement();
+    requestAnimationFrame(start)
+  }
+  else {
+    back();
+  }
+}
+function back() {
+  if (hero.left > 0) {
+    hero.moveLeft();
+    document.getElementById('game').innerHTML = hero.getHeroElement();
+    requestAnimationFrame(back)
+  }
+  else {
+    start();
+  }
+}
+function Up() {
+  console.log("TCL: start -> window.innerWidth-hero.size", window.innerWidth - hero.size);
+  if (hero.top < height) {
+    hero.moveDown();
+    document.getElementById('game').innerHTML = hero.getHeroElement();
+    requestAnimationFrame(Up)
+  }
+  else {
+    Down();
+  }
+}
+function Down() {
+  if (hero.top > 0) {
+    hero.moveUp();
+    document.getElementById('game').innerHTML = hero.getHeroElement();
+    requestAnimationFrame(Down)
+  }
+  else {
+    Up();
+  }
+}
+
+start();
+Up();
